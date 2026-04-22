da1c4eaa6b1bbccdd7d42e67910a1284affd7584
AmrrHarharah <amr0112@student.abudzar.sch.id>
2026-04-22 09:04:48 +0700

    ujian amr

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-const box1 = document.getElementById("box1");
+const mode = document.getElementById("mode");
 const text1 = document.getElementById("text1");
 const black = document.getElementById("black");
-box1.addEventListener("click", function () {
+mode.addEventListener("click", function () {
   alert("tombol di klik");
   document.body.classList.toggle("dark-mode");
   document.body.classList.toggle("black");
